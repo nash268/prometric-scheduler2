051157fdf777864f288e6c869d4ced922bd4621b
Nesh Kumar <130772656+nash268@users.noreply.github.com>
2024-03-07 11:37:19 +0500

    version 0.1

--- utils/proscheduler.py
+++ utils/proscheduler.py
@@ -10,6 +10,7 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import Select
 
 class Proscheduler():
   def start(self):
