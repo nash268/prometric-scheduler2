a66b7c936ee3ddead5b74966c3ca46367bf0acd1
Nesh Kumar <130772656+nash268@users.noreply.github.com>
2025-02-22 22:01:54 +0500

    update

--- utils/taskmanager.py
+++ utils/taskmanager.py
@@ -9,17 +9,14 @@ class TaskManager:
         if (platform.system() == "Windows"):
             # Command to query existing tasks
             query_command = f'schtasks /query /tn "{task_name}"'
-            
             # Execute the command and capture the output
             result = subprocess.run(query_command, shell=True, capture_output=True, text=True)
-
             # Return True if task name is found in the output
             if (task_name in result.stdout):
                 return True
             else:
                 return False
-
-        if (platform.system() == "Linux" or operating_system == "Darwin"):
+        elif (platform.system() == "Linux" or operating_system == "Darwin"):
             query_command = f'crontab -l | grep "{task_name}"'
             result = subprocess(query_command, shell=True, capture_output=True, text=True)
             if (task_name in result.stdout):
@@ -32,8 +29,7 @@ class TaskManager:
             # Command to delete the task on windows
             command = f'schtasks /delete /tn "{task_name}" /f'
             subprocess.run(command, shell=True)
-
-        if (platform.system() == "Linux" or operating_system == "Darwin"):
+        elif (platform.system() == "Linux" or operating_system == "Darwin"):
             # list all tasks, removes line containing task name, rewrite crontab file.
             command = f'crontab -l | grep -v "{task_name}" | crontab -'
             subprocess.run(command, shell=True)
