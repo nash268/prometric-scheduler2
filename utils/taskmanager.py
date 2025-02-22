e4184db2a088829663916e224bd078ceee36e648
Nesh Kumar <130772656+nash268@users.noreply.github.com>
2025-02-22 16:09:04 +0500

    update

--- utils/taskmanager.py
+++ utils/taskmanager.py
@@ -1,56 +1,45 @@
+import platform
 import subprocess
 
-# class for managing tasks on windows
-class WindowsTasks:
+class TaskManager:
     def __init__(self):
         pass
 
-    def exists(self, task_name):
-        # Command to query existing tasks
-        query_command = f'schtasks /query /tn "{task_name}"'
-        
-        # Execute the command and capture the output
-        result = subprocess.run(query_command, shell=True, capture_output=True, text=True)
-        
-        # Check if the task name is found in the output
-        return task_name in result.stdout
-    
-    def delete(self, task_name):
-        # Command to delete the task
-        command = f'schtasks /delete /tn "{task_name}" /f'
-        subprocess.run(command, shell=True)
-
-    def create(self, task_name, schedule, script_path, script_name):
-        
-        # Command to schedule the task
-        command = f'schtasks /create /sc minute /mo {schedule} /tn "{task_name}" /tr "cmd /c cd /d {script_path} && py {script_name}"'
-
-        # Execute the command to create the task
-        subprocess.run(command, shell=True)
-
-
-
-class CronJobs:
-    def __init__(self):
-        pass
-
-    def delete(self):
-        # Command to remove the cron job
-        delete_command = f'crontab -r'
-        subprocess.run(delete_command, shell=True)
-        print("SUCCESS: all previous cronjobs deleted.")
-
-    def create(self, operating_system, schedule, script_path, script_name):
-        if operating_system == "Linux":
-            # for Linux, the DISPLAY environment variable is used for GUI applications.
-            # Find the display value
-            display = subprocess.check_output(["echo", "$DISPLAY"]).decode().strip()
-            # command for setting up cronjob
-            command = f'(echo "{schedule} export DISPLAY={display}; cd {script_path} && python3 {script_name} >> {script_path}/logfile 2>&1") | crontab -'
-
-        elif operating_system == "Darwin":
-            # cron jobs on macOS can work without setting the DISPLAY environment variable
-            command = f'(echo "{schedule} cd {script_path} && python3 {script_name} >> {script_path}/logfile 2>&1") | crontab -'
+    def task_status(self, task_name):
+        if (platform.system() == "Windows"):
+            # Command to query existing tasks
+            query_command = f'schtasks /query /tn "{task_name}"'
             
-        subprocess.run(command, shell=True)
-        print("SUCCESS: prometric cronjob created successfully.")
+            # Execute the command and capture the output
+            result = subprocess.run(query_command, shell=True, capture_output=True, text=True)
+
+            # Return True if task name is found in the output
+            if (task_name in result.stdout):
+                return True
+            else:
+                return False
+
+        if (platform.system() == "Linux" or operating_system == "Darwin"):
+            query_command = f'crontab -l | grep "{task_name}"'
+            result = subprocess(query_command, shell=True, capture_output=True, text=True)
+            if (task_name in result.stdout):
+                return True
+            else:
+                return False
+
+    def delete_task(self, task_name):
+        if (platform.system() == "Windows"):
+            # Command to delete the task on windows
+            command = f'schtasks /delete /tn "{task_name}" /f'
+            subprocess.run(command, shell=True)
+
+        if (platform.system() == "Linux" or operating_system == "Darwin"):
+            # list all tasks, removes line containing task name, rewrite crontab file.
+            command = f'crontab -l | grep -v "{task_name}" | crontab -'
+            subprocess.run(command, shell=True)
+
+    def create_task(self, task_name, schedule, script_path):
+        if (platform.system() == "Windows"):
+            # Command to schedule the task
+            command = f'schtasks /create /sc minute /mo {schedule} /tn "{task_name}" /tr "cmd /c cd /d {script_dir} && py {script_name}"'
+            subprocess.run(command, shell=True)
